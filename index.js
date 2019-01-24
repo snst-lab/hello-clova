b90a628fb0eba201445406b9ac156c3dafb2f678
snst-lab <snst.lab@gmail.com>
2019-01-24 22:56:52 +0900

    update

---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict';
+const express = require('express');
+const clova = require('@line/clova-cek-sdk-nodejs');
+const linebot = require('@line/bot-sdk');
+const redis = require('redis');
+
+/**
+ * Configure Redis
+ */
+if (process.env.REDISTOGO_URL) {
+    const rtg    = require('url').parse(process.env.REDISTOGO_URL);
+    var db = redis.createClient(rtg.port, rtg.hostname);
+    db.auth(rtg.auth.split(':')[1]);
+} else {
+    var db = redis.createClient();
+}
+db.on('error', err => {
+    console.log('Error: ' + err);
+});
+
+
+/**
+ * Configure ClovaSkill
+ */
+var MESSAGE;
+
+const clovaSkillHandler = clova.Client.configureSkill()
+.onLaunchRequest(responseHelper => {
+      const rand = (min, max) => ~~(Math.random() * (max - min + 1) + min);
+      const wakeup =[
+          'Hello!',
+          'Yah!',
+          'Hey! What is up?'
+      ];
+      responseHelper.setSimpleSpeech(
+          clova.SpeechBuilder.createSpeechText(wakeup[rand(0,wakeup.length-1)],'en')
+      );
+})
+.onIntentRequest(async responseHelper => {
+    db.get('message', (err,reply)=>{
+        MESSAGE=reply;
+    });
+    const SpeechList = Array(5).fill().map(e=>clova.SpeechBuilder.createSpeechUrl('https://raw.githubusercontent.com/snst-lab/hello-clova/master/assets/audio/3sec.mp3'));
+    SpeechList[0] = clova.SpeechBuilder.createSpeechText(MESSAGE||'Are you crazy?','en');
+    responseHelper.setSpeechList(SpeechList);
+})
+.onSessionEndedRequest(responseHelper => {})
+.handle();
+
+
+/**
+ * Configure LineBot
+ */
+const botConfig = {
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+const bot = new linebot.Client(botConfig);
+
+const linebotHandler = (event) => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+  return db.set('message', event.message.text, (err, reply)=>{
+       bot.replyMessage(event.replyToken, {
+         type: 'text',
+         text: reply
+      });
+  });
+}
+
+
+/**
+ * Configure Server
+ */
+const app = new express();
+app.post('/bot', linebot.middleware(botConfig), (req, res) => {
+  Promise
+    .all(req.body.events.map(linebotHandler))
+    .then((result) => res.json(result));
+});
+app.post('/clova', clova.Middleware({ applicationId: "com.snst.helloclova" }), clovaSkillHandler);
+app.get('/', (req, res)=>{
+    res.send('Hello Clova!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on ${port}`));
