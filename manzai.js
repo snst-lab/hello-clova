ba7c106cd5176acc801624a7b3132a46bba97b19
snst-lab <snst.lab@gmail.com>
2019-01-26 23:31:30 +0900

    update

--- manzai.js
+++ manzai.js
@@ -1,7 +1,7 @@
 const manzai = {
-    0:['Hello,AUDIENCE. Nice to meet you.',0,0,0],
+    0:['Hello,AUDIENCE. Nice to meet you.',0,0],
     1:['Oh, really? Thank you AUDIENCE.',0,0],
-    2:['Hey,AUDIENCE?','Shunsuke is a sloppy person.',' When He is working on something, he is unable to care about other things.','And he is a dirty man.',0,0,0],
+    2:['Hey,AUDIENCE?','Shunsuke is a sloppy person.',' When He is working on something, he is unable to care about other things.','And he is a dirty man.',0,0],
     3:[0,0,0,0,0,0,0],
     4:['Because you said me shut up.',0,0],
     5:['Good-bye AUDIENCE.','See you again.']
